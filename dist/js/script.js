bfdefe474b817f3ba90033ea1966dac0e072dd6e
nugraha-22 <nugrahahungkul27@gmail.com>
2023-01-06 13:12:41 +0700

    Change All CSS

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,16 @@
-const navbar = document.querySelector('.navbar');
+const navBar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
     const positionWindow = window.scrollY > 0;
-    navbar.classList.toggle('active', positionWindow);
+    navBar.classList.toggle('active', positionWindow);
 });
+
+const btnMenuBar = document.querySelector('.menu-bars');
+const menuBar = document.querySelector('.navigation');
+
+btnMenuBar.addEventListener('click', () => {
+    menuBar.classList.toggle('active');
+})
 
 const items = document.querySelector('.items');
 const listItems = document.querySelector('.list-items');
@@ -12,46 +19,32 @@ items.addEventListener('click', () => {
     listItems.classList.toggle('active');
 });
 
-const menuBar = document.querySelector('.menu-bars');
-const sideBar = document.querySelector('.navigation');
-
-menuBar.addEventListener('click', () => {
-    sideBar.classList.toggle('active');
-});
-
 const closeBar = document.querySelector('.fa-circle-xmark');
 
 closeBar.addEventListener('click', () => {
-    sideBar.classList.remove('active');
+    menuBar.classList.remove('active');
 });
 
 const hero = document.querySelector('.hero');
-
 hero.addEventListener('click', () => {
-    sideBar.classList.remove('active');
+    menuBar.classList.remove('active');
 });
-
 const about = document.querySelector('.about');
-
 about.addEventListener('click', () => {
-    sideBar.classList.remove('active');
+    menuBar.classList.remove('active');
+    
 });
-
 const certificate = document.querySelector('.certificate');
-
 certificate.addEventListener('click', () => {
-    sideBar.classList.remove('active');
+    menuBar.classList.remove('active');
+    
 });
-
 const project = document.querySelector('.project');
-
 project.addEventListener('click', () => {
-    sideBar.classList.remove('active');
+    menuBar.classList.remove('active');
+    
 });
-
 const contact = document.querySelector('.contact');
-
 contact.addEventListener('click', () => {
-    sideBar.classList.remove('active');
+    menuBar.classList.remove('active');
 });
-
